0eff00aa38ba68bb82593c0e51b3968b61f9e535
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-04-02 15:36:42 +0200

    Simple sequence searcher added

---
+++ src/main/java/edu/iis/mto/search/SequenceSearcherExample.java
@@ -0,0 +1,23 @@
+package edu.iis.mto.search;
+
+public class SequenceSearcherExample implements SequenceSearcher {
+
+    @Override public SearchResult search(int elem, int[] seq) {
+        if(seq == null) throw new IllegalArgumentException();
+
+        SearchResult.Builder builder = SearchResult.builder();
+        builder.withFound(false);
+        builder.withPosition(-1);
+        for (int i=0;i<seq.length;i++) {
+            if(seq[i] == elem) {
+                builder.withFound(true);
+                builder.withPosition(i);
+                break;
+            }
+
+        }
+
+        return builder.build();
+
+    }
+}
