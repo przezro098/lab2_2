7e6513518dde7555407799c1a8112e4090290b4c
Tomasz Kucharczyk <215786@edu.p.lodz.pl>
2020-04-02 16:17:50 +0200

    State test Checking similarity of empty sequences

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,7 +1,31 @@
 package edu.iis.mto.similarity;
 
+import edu.iis.mto.search.SequenceSearcher;
+import edu.iis.mto.search.SequenceSearcherExample;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityFinderTest {
+
+    private SequenceSearcher sequenceSearcher = new SequenceSearcherExample();
+    private SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcher);
+    private int[] firstSequence;
+    private int[] secondSequence;
+    private double result;
+
+    @Test
+    void CheckingBothEmptySequence() {
+        firstSequence = new int[]{};
+        secondSequence = new int[]{};
+
+        result = similarityFinder.calculateJackardSimilarity(firstSequence,secondSequence);
+
+        Assertions.assertEquals(1.0, result);
+
+
+    }
+
 
 }
